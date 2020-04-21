18df3b72e22ba2ff140d8a1ffa704e7682b937ac
Yaroslav <dombrovskiy91@gmail.com>
2020-04-22 00:00:02 +0300

    new component Dialogs

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import  Header from './Components/Header/Header';
 import Aside from './Components/Aside/Aside';
-import Profile from './Components/Profile/Profile';
+// import Profile from './Components/Profile/Profile';
+import Dialogs from './Components/Dialogs/Dialogs';
 function App() {
   return (
     <div className="App-wrapper">
       <Header/>
       <Aside/>
-      <Profile/>
+      <Dialogs/>
+      {/* <Profile/> */}
     </div>
   );
 }
