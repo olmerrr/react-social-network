758fc31a8c878f14e825c8579a873796f71c6289
Yaroslav <dombrovskiy91@gmail.com>
2020-04-26 17:04:50 +0300

    .map

--- src/Components/Profile/MyPosts/MyPosts.js
+++ src/Components/Profile/MyPosts/MyPosts.js
@@ -3,14 +3,15 @@ import MyPostsStyle from './MyPosts.module.css';
 import Post from './Post/Post';
 
 
-const MyPost = () =>{
+const MyPost = (props) =>{
+
   let postsData = [
     {id: 1, message: 'Give more JSX', likeCount: 5},
     {id: 2, message: 'Hello Friend!!', likeCount: 33},
-    {id: 3, message: 'Its new component?', likeCount: 0}
+    {id: 3, message: 'Why?', likeCount: 0}
   ];
   let postsElements =  postsData.map( (post) => 
-  <Post id = {post.id} 
+  <Post     id = {post.id} 
             message = {post.message} 
             likeCount = {post.likeCount}
   />
@@ -18,12 +19,12 @@ const MyPost = () =>{
     
 return(
 <div>
-    <p className={MyPostsStyle.newStylePost}> This is real post from Component</p>
-    <div className = {MyPostsStyle.coment_container}>
-      <textarea>Some text</textarea>
-      <button>Add post</button>
-    </div>
-    {postsElements}
+     <p className={MyPostsStyle.newStylePost}> This is real post from Component</p>
+    {/* <div className = {MyPostsStyle.coment_container}> */}
+      {/* <textarea>Some text</textarea> */}
+      {/* <button>Add post</button> */}
+    {/* </div> */}
+   {postsElements} 
   </div>  
   )
 }
