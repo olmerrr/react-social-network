9389cabe7038412b197a5aa0246521eb657f0425
Yaroslav <dombrovskiy91@gmail.com>
2020-05-08 19:23:49 +0300

    action crator + action type

--- src/Components/Profile/MyPosts/MyPosts.js
+++ src/Components/Profile/MyPosts/MyPosts.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import s from './MyPosts.module.css';
 import Post from './Post/Post';
-
+import {addPostActionCreator, updateNewPostTextActionCreator} from '../../../redux/store';
 const MyPosts = (props) => {
   let postsElements = props.posts.map( p=> <Post message = {p.message} likeCounts = {p.likeCounts}/>)
-  
   let newPostElement = React.createRef();   //создаем ссылку на DOM элемент
-  
-  let addPost  = () =>{
-    props.addPost();
 
-  } ;
+
+
+  let addPost  = () =>{
+    props.dispatch(addPostActionCreator());
+  };
+  
   let onPostChange = () => {
     let text = newPostElement.current.value; 
-    props.updateNewPostText(text);
+    let action = updateNewPostTextActionCreator(text);
+    props.dispatch(action);
   };
-
   return (
     <div>
       My posts
--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,3 +1,5 @@
+const ADD_POST ='ADD-POST' ;
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
 
 let  store = {
   _state : {
@@ -29,32 +31,40 @@ let  store = {
       ]
     }
   },
+  _callSubscriber () {
+    console.log('State is changed');
+  },
+
   getState (){
     return this._state;
   },
-  callSubscriber () {
-    console.log('State is changed');
-  },
- addPost  (){
-  const newPost = {
-      id: 5,
-      
-      message: this._state.profilePage.newPostText,
-      likeCounts: 112
-    };
-    this._state.profilePage.posts.push(newPost);  
-    this._state.profilePage.newPostText = ' ';
-    this._callSubscriber(this._state);
-  },
-  updateNewPostText (newText){
-    this._state.profilePage.newPostText = newText;
-    this._callSubscriber(this._state);
-  },
   subscribe  (observer){
     this._callSubscriber = observer; //patern
-  }
+  },
+  dispatch(action){
+    if (action.type === ADD_POST){
+    let newPost = {
+        id: 5,
+        message: this._state.profilePage.newPostText,
+        likeCounts: 112
+      };
+      this._state.profilePage.posts.push(newPost);  
+      this._state.profilePage.newPostText = ' ';
+      this._callSubscriber(this._state);
+      } else if (action.type === UPDATE_NEW_POST_TEXT){
+      this._state.profilePage.newPostText = action.newText;
+      this._callSubscriber(this._state);
+    }
+  } 
 };
 
+
+
+export const addPostActionCreator = () =>({ type: ADD_POST });
+
+export const updateNewPostTextActionCreator = (text) =>{
+  return {type: UPDATE_NEW_POST_TEXT,newText: text}
+}; 
 export default store;
 window.store = store;
 
